42b306fa8e59af0b766c6ec9a8eb2d2811657b1d
Sycend <sycend@web.de>
2017-07-12 20:40:51 +0200

    Changed Timer to Storage
    Changed Timersystem to the storage system.
    also added missing Else case and some perfomance tweaks

--- src/background.js
+++ src/background.js
@@ -1,25 +1,24 @@
 var affiliateTag = "tag=beskarschmied-21";
-
 var amazonAddress = "amazon.de";
+var simpleStorage = require("sdk/simple-storage");
 
 browser.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
-    var currentTime = Date();
-
-    if(timers[tabId] == null || (currentTime - timers[tabId]) === (20 * 60 * 1000))
-    {
-      timers[tabId] = currentTime;
-
-      var tabUrl = tab.url;
-      if(tabUrl.indexOf(affiliateTag) == -1) {
-        if(tabUrl.indexOf(amazonAddress) > -1) {
-          if(tabUrl==amazonAddress||tabUrl==amazonAddress+"/") {
-            tabUrl += "?"+affiliateTag; 
-          }
-          tabUrl += "&"+affiliateTag;
-          browser.tabs.update(tabId, { url: tabUrl} );
-        }
-      }
-    }
-  }
+	function(tabId, changeInfo, tab) {
+		var tabUrl = tab.url;
+		if(tabUrl.indexOf(affiliateTag) == -1) {
+			if(tabUrl.indexOf(amazonAddress) > -1) {
+				var lastTime = simpleStorage.storage.lastTime;
+				var currentTime = Date();
+				if(simpleStorage.storage.lastTime = null || (currentTime - lastTime === (20 * 60 * 1000))){
+					if(tabUrl == amazonAddress || tabUrl == amazonAddress + "/") {
+						tabUrl += "?" + affiliateTag; 
+					} else {
+						tabUrl += "&" + affiliateTag;
+					}
+					browser.tabs.update(tabId, { url: tabUrl} );
+					simpleStorage.storage.lastTime = currentTime;
+				}
+			}
+		}
+	}
 );
