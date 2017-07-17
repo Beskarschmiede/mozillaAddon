fe7397abf56344797152777abf8fa688339b04ee
Daniel Igel <maddeye@arcor.de>
2017-07-17 21:41:54 +0200

    update loop

--- src/background.js
+++ src/background.js
@@ -1,23 +1,24 @@
 var affiliateTag = "tag=beskarschmied-21";
 var amazonAddress = "amazon.de";
-var simpleStorage = require("sdk/simple-storage");
 
 browser.tabs.onUpdated.addListener(
 	function(tabId, changeInfo, tab) {
 		var tabUrl = tab.url;
-		if(tabUrl.indexOf(affiliateTag) == -1) {
+		if(tabUrl.indexOf(affiliateTag) === -1) {
 			if(tabUrl.indexOf(amazonAddress) > -1) {
-				var lastTime = simpleStorage.storage.lastTime;
-				var currentTime = Date();
-				if(lastTime = null || (currentTime - lastTime === (20 * 60 * 1000))){
-					if(tabUrl == amazonAddress || tabUrl == amazonAddress + "/") {
-						tabUrl += "?" + affiliateTag; 
-					} else {
-						tabUrl += "&" + affiliateTag;
+				browser.storage.local.get("lastTime", function(data){
+					var lastTime = isNaN(data.lastTime) ? null : new Date(parseInt(data.lastTime));
+					var currentTime = new Date();
+					if(lastTime === null || (currentTime.getTime() - lastTime.getTime()) > (20 * 60 * 1000)){
+						if(tabUrl.indexOf("?") === -1) {
+							tabUrl += "&" + affiliateTag;
+						} else {
+							tabUrl += "?" + affiliateTag;
+						}
+						browser.storage.local.set({'lastTime': +new Date()});
+						browser.tabs.update(tabId, { url: tabUrl} );
 					}
-					browser.tabs.update(tabId, { url: tabUrl} );
-					simpleStorage.storage.lastTime = currentTime;
-				}
+				});
 			}
 		}
 	}
